5705168a6a8b3d487914a41e94e7982fdfbd2e1d
Vianney Ribeiro <vianneyribeiro@macbook-pro-de-vianney.local>
2017-02-11 10:55:34 -0300

    .

---
+++ Pods/Target Support Files/Pods-Ponto/Pods-Ponto-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ponto : NSObject
+@end
+@implementation PodsDummy_Pods_Ponto
+@end
---
+++ Pods/Target Support Files/Pods-PontoUITests/Pods-PontoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PontoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PontoUITests
+@end
